b655e5c0cd0cb538e197da73daba49c600e356b0
Théo Zimmermann <theo.zimmermann@ens.fr>
2012-01-05 10:12:50 +0100

    Rw et Sw

--- licsAst.ml
+++ licsAst.ml
@@ -12,13 +12,8 @@ type expression =
 
 type stmt =
     Assign of int * expression
-  | Inputreg of int | Input of int
-  | Outputreg of int | Output of int
-  | Inputram of int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int
-(** Les 32 premiers bits indiquent les registres où sera stockée l'information (équivalent d'un unique registre MIPS 32 bits.
-   Les 8 derniers indiquent l'adresse de lecture (donc on a une RAM de 256 * 4 octets)
-   Même topo pour Outputram sauf qu'on écrit au lieu de lire *)
-  | Outputram of int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int * int
+  | Lw of string * (int list) | Inputreg of int | Input of int
+  | Sw of string * (int list) | Outputreg of int | Output of int
 
 type program = stmt list
 
